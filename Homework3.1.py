4dba05ce6c33006826506ff5b4f6ff5b5bca03a1
Elater35 <mkvpsh13@gmail.com>
2026-06-02 16:23:35 +0300

    Homework3.1_1.py

--- Homework3.1.py
+++ Homework3.1.py
@@ -2,15 +2,19 @@
 
 number1_math_operation = int(input("Enter the first number of the mathematical operation: "))
 number2_math_operation = int(input("Enter the second number of the mathematical operation: "))
-action_math_operation = input("Enter the action of the mathematical operation: ")
+action_math_operation = input("Enter the action of the mathematical operation: +, -, * or /: ")
 
-if action_math_operation == "+":
+if action_math_operation != "+" and action_math_operation != "-" and action_math_operation != "*" and action_math_operation != "/":
+    print("Error. Incorrect action.")
+
+elif action_math_operation == "+":
     print(str(number1_math_operation), "+", str(number2_math_operation), "=", number1_math_operation + number2_math_operation, end=" \n \n")
 elif action_math_operation == "-":
     print(str(number1_math_operation), "-", str(number2_math_operation), "=", number1_math_operation - number2_math_operation, end=" \n \n")
 elif action_math_operation == "*":
     print(str(number1_math_operation), "*", str(number2_math_operation), "=", number1_math_operation * number2_math_operation, end=" \n \n")
+
 elif number2_math_operation == 0:
-    print("Error. Numbers are not divisible by zero.", end=" \n \n")
+    print("Error. Numbers are not divisible by zero.")
 else:
     print(str(number1_math_operation), "/", str(number2_math_operation), "=", number1_math_operation / number2_math_operation, end=" \n \n")
