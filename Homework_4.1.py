8a7d24032d79fa6ac288fe13d51d9ed62eb7e084
Elater35 <mkvpsh13@gmail.com>
2026-06-05 13:43:30 +0300

    Homework_4.1.py.py

--- Homework_4.1.py
+++ Homework_4.1.py
@@ -1,19 +1,21 @@
 # Перемістити всі нулі до кінця списку.
 
 input_lst = [2, 0, 0, 16, 0, 3, 27, 0, 5, 318, 7, 55, 9, 0]
+print(input_lst)
+# input_lst = [2, 16, 3, 27, 5, 318, 7, 55, 9]
+# input_lst = [2, 16, 3, 27, 0, 5, 318, 7, 55, 9, 0, 0, 0, 0, 0]
 
-number_zeros_of_lst = 0 # лічильник циклу
+number_zeros_of_lst = input_lst.count(0)
+print(number_zeros_of_lst)
+cycle_counter = 0
 
-while number_zeros_of_lst < input_lst.count(0):
-    input_lst.index(0)
-if length_lst == 1:
-    result_lst = [input_lst, []]
-elif length_lst != 0 and length_lst % 2 == 0:
-    result_lst = [input_lst[:length_lst // 2], input_lst[length_lst // 2:]]
-elif length_lst % 2 == 1:
-    result_lst = [input_lst[:length_lst // 2 + 1], input_lst[length_lst // 2 + 1:]]
-else:
-    result_lst = [[], []]
+# for cycle_counter == 0
+while cycle_counter < number_zeros_of_lst:
+    ind = input_lst.index(0)
+    if ind == len(input_lst) - number_zeros_of_lst:
+        break
+    input_lst.insert(-1,0)
+    input_lst.pop(number_zeros_of_lst)
+    cycle_counter += 1
 
 print(input_lst)
-print(result_lst)
