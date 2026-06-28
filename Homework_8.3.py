dd176c4b1cf59323e7f3e153b0a2d7fa0b07ea93
Elater35 <mkvpsh13@gmail.com>
2026-06-28 11:49:19 +0300

    Homework_8.3.py

---
+++ Homework_8.3.py
@@ -0,0 +1,20 @@
+# ДЗ 8.3. Унікальне число.
+
+def find_unique_value(some_list):
+
+    unique_number = None
+
+    for item in some_list:
+        if some_list.count(item) == 1:
+            unique_number = item
+            break
+
+    return unique_number
+
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+assert find_unique_value([5, 5, 5, 2, 2]) is None, 'Test4'
+print("ОК")
+
