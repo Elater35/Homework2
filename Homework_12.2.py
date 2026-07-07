165d1418fd758ae8c8fbe05c196e511755118104
Elater35 <mkvpsh13@gmail.com>
2026-07-07 17:46:47 +0300

    Homework_12.1.py

---
+++ Homework_12.2.py
@@ -0,0 +1,15 @@
+# ДЗ 12.2. Заповнення списку кубами чисел
+
+def generate_cube_numbers(end):
+
+    for item in range(2, int(end ** (1 / 3) + 0.001) + 1):
+        yield item ** 3
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print("OK")
