078a5ef6387921d37ce328a6e86e1f602c0b0ce0
Elater35 <mkvpsh13@gmail.com>
2026-07-12 17:06:54 +0300

    Homework_13.1.py

---
+++ Homework_13.1.py
@@ -0,0 +1,35 @@
+# ДЗ 13.1. Очистити текст від html-тегів
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with open(html_file, 'r', encoding='utf-8') as file:
+        html = file.read()
+
+
+    def clean_html(html_text):
+        if html_text.find("<") == -1:
+            return html_text
+        else:
+            text = (html_text[:html_text.find("<")] + html_text[html_text.find(">") + 1:]).replace("  ", " ")
+            return clean_html(text)
+
+
+    text_res = clean_html(html)
+
+    with open(result_file, 'w', encoding='utf-8') as file_1:
+        file_1.write(text_res)
+
+
+def delete_empty_str(cleaned_file, result_file_1='cleaned_res.txt'):
+    with open(cleaned_file, 'r', encoding='utf-8') as file_1:
+        draft_text = file_1.readlines() # для невеликих файлів використовуємо "readlines()"
+
+    draft_text = [item for item in draft_text if item not in ("\n", " \n")]
+    cleaned_file_txt = "".join(draft_text)
+
+    with open(result_file_1, 'w', encoding='utf-8') as file_2:
+        file_2.write(cleaned_file_txt)
+
+
+delete_html_tags("draft.html")
+delete_empty_str("cleaned.txt")
